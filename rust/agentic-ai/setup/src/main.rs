31b9f3b547239e586e2b97b2a02bfc192a80e0f6
Ravi Shankar <ravishankar@Ravis-Personal-Macbook-pro.local>
2026-03-14 22:41:59 +0530

    rust setup code updated

--- rust/agentic-ai/setup/src/main.rs
+++ rust/agentic-ai/setup/src/main.rs
@@ -1,12 +1,24 @@
 use dotenvy::dotenv_override;
 use std::env;
 
-fn main() {
+use gemini_client_api::gemini::ask::Gemini;
+use gemini_client_api::gemini::types::sessions::Session;
+
+#[tokio::main]
+async fn main() {
     // Please ensure you are putting in the actual path of the .env file as mentioned in the README, and not the placeholder path shown below.
     dotenvy::from_path("../.env").ok(); 
 
     let api_key = env::var("GEMIN_API_KEY")
         .expect("GEMIN_API_KEY must be set in the workspace root .env");
         
-    println!("Key loaded: {}", &api_key[..4]); 
+    let mut session = Session::new(10); // Keep last 10 messages
+    let ai = Gemini::new(
+        api_key, 
+        "gemini-2.5-flash", 
+        None, // Optional system instruction
+    );
+
+    let response = ai.ask(session.ask("Hello, Gemini!")).await.unwrap();
+    println!("Gemini: {}", response.get_chat().get_text_no_think("\n"));
 }
